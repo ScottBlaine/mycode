40dfacaaf264a7b804d3b8aa00b15d445548153b
ScottBlaine <scott.blaine@Stellantis.com>
2021-04-26 18:38:24 +0000

    list work

---
+++ lab_input/day_input.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+user_Name = input("Please enter your name:")
+
+Day_of_Week = input("Please enter the day of the week:")
+
+print("Hello, ",user_Name, "Happy", Day_of_Week)
+
---
+++ mix-list/mixlist01.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+#Create a list with three items
+my_list = [ "192.168.0.5", 5060, "UP" ]
+
+#Print out the first item
+print("The first item in the list (IP): " + my_list[0] )
+
+#print out the second item after converting to string
+print("The second item in the list (port): " + str(my_list[1]) )
+
+#print out list item
+
+print("The last item in the list (state): " + my_list[2] )
+
+iplist = [5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh"]
+
+print(f" The IP address in the list is:{iplist[3]}") 
+
